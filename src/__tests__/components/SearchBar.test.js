74e9dd79cd9cace3c8b27ac339c8a396879440dc
Igor Souza <igorsouza.dev@gmail.com>
2020-01-28 13:01:11 -0300

    Added more coverage to the search bar

--- src/__tests__/components/SearchBar.test.js
+++ src/__tests__/components/SearchBar.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
 import SearchBar from 'components/SearchBar';
@@ -16,5 +16,17 @@ describe('<SearchBar />', () => {
     const linkElement = getByTestId(/search-bar/i);
     expect(linkElement).toBeInTheDocument();
     expect(container.firstChild).toMatchSnapshot();
+  });
+  it('should navigate to the details page when clicked', () => {
+    const history = createMemoryHistory();
+    const { container, getByTestId } = render(
+      <Router history={history}>
+        <SearchBar />
+      </Router>
+    );
+    fireEvent.change(container.querySelector('input[type=text]'), { target: { value: 'test' } });
+    fireEvent.click(getByTestId('search-bar').querySelector('button'));
+    const expectedUrl = `${history.location.pathname}${history.location.search}`;
+    expect(expectedUrl).toEqual('/search?q=test');
   });
 });
